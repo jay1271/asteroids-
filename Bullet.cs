e317af2728c8f899a9905c54936a1205ae6fa479
jay1271 <76547546+jay1271@users.noreply.github.com>
2021-10-17 14:27:59 +0530

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float maxlifetime = 10.0f;
+    public float speed = 500.0f;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    public void Project (Vector2 direction)
+    {
+        _rigidbody.AddForce(direction * this.speed);
+        Destroy(this.gameObject, this.maxlifetime);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+
+
+}
